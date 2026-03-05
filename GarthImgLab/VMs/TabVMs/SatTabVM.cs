9bdfd48c1cb816b03eb7885160c87e69978e6bf2
Garth TB | 天卜 <g-art-h@outlook.com>
2026-03-05 21:50:07 +0800

    添加SatTabVM类

---
+++ GarthImgLab/VMs/TabVMs/SatTabVM.cs
@@ -0,0 +1,21 @@
+namespace GarthImgLab.VMs.TabVMs;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using Core;
+using ImageMagick;
+
+internal sealed partial class SatTabVM: FXTabVM
+{
+    [ObservableProperty] private bool _antiClip = true;
+    [ObservableProperty] private double _gain;
+    [ObservableProperty] private string _strat = "";
+    public static IReadOnlyCollection<string> Strats => Sat.Adjustors.Keys;
+    partial void OnGainChanged(double value) => Gain = Math.Clamp(value, -1, 1);
+
+    public override void Apply(IMagickImage<ushort> img, CancellationToken token) =>
+        Dialog.RunOrShowEx(
+            "调整饱和度",
+            () => {
+                if (Gain != 0) img.MapRgb(Sat.Adjustors[Strat](Gain), AntiClip, token);
+            });
+}
