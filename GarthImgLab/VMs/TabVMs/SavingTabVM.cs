ff474c0d00329340921276c3039e47380fb21fb2
Garth TB | 天卜 <g-art-h@outlook.com>
2026-03-05 21:42:05 +0800

    添加SavingTabVM类

---
+++ GarthImgLab/VMs/TabVMs/SavingTabVM.cs
@@ -0,0 +1,17 @@
+namespace GarthImgLab.VMs.TabVMs;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using Core;
+
+internal sealed partial class SavingTabVM: ObservableObject
+{
+    [ObservableProperty] private string _format = "", _option = "";
+    [ObservableProperty] private string[] _options = [];
+    [ObservableProperty] private byte _quality = 96;
+    public static IReadOnlyCollection<string> Formats => Saving.Options.Keys;
+
+    partial void OnFormatChanged(string value) {
+        if (!string.IsNullOrWhiteSpace(value))
+            Option = (Options = Saving.Options[value]).FirstOrDefault("");
+    }
+}
