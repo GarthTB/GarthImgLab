b6c542e96343ed8c88a54c3d5cdddace4c9e8a44
Garth TB | 天卜 <g-art-h@outlook.com>
2026-03-06 00:40:14 +0800

    添加MainWindow拖放和预览功能

--- GarthImgLab/Views/MainWindow.xaml.cs
+++ GarthImgLab/Views/MainWindow.xaml.cs
@@ -1,6 +1,35 @@
 ﻿namespace GarthImgLab.Views;
 
+using System.IO;
+using System.Windows;
+using VMs;
+
 public sealed partial class MainWindow
 {
-    public MainWindow() => InitializeComponent();
+    private readonly PreviewWindow _previewWindow = new();
+
+    public MainWindow() {
+        InitializeComponent();
+        DataContext = new MainVM((PreviewVM)_previewWindow.DataContext);
+        Closing += (_, _) => _previewWindow.Shut();
+        Closed += (_, _) => (DataContext as IDisposable)?.Dispose();
+    }
+
+    private void IconPathDrop(object s, DragEventArgs e) {
+        if (DataContext is MainVM { FramingTabVM: var vm })
+            Dialog.RunOrShowEx(
+                "拖放图标",
+                () => {
+                    if (e.Data.GetData(DataFormats.FileDrop) is string[] and [var path, ..])
+                        (vm.Icon, vm.UseIcon) = (new(path), true);
+                },
+                () => vm.Icon = null);
+    }
+
+    private void ImgPathsDrop(object s, DragEventArgs e) {
+        if (DataContext is MainVM vm && e.Data.GetData(DataFormats.FileDrop) is string[] paths)
+            vm.AddImg(paths.Where(File.Exists));
+    }
+
+    private void ShowPreview(object s, RoutedEventArgs e) => _previewWindow.Show();
 }
