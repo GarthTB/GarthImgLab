d89d154d4417855d380d3f18db61896d5c740f94
Garth TB | 天卜 <g-art-h@outlook.com>
2026-03-06 21:30:20 +0800

    实现PreviewVM和MainVM的预览与批处理功能

--- GarthImgLab/VMs/MainVM.cs
+++ GarthImgLab/VMs/MainVM.cs
@@ -1,5 +1,78 @@
 namespace GarthImgLab.VMs;
 
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Core;
+using TabVMs;
 
-internal sealed class MainVM: ObservableObject;
+internal sealed partial class MainVM(PreviewVM previewVM): ObservableObject, IDisposable
+{
+    [ObservableProperty] private byte _tabIdx;
+    public SatTabVM SatTabVM { get; } = new();
+    public FramingTabVM FramingTabVM { get; } = new();
+    public SavingTabVM SavingTabVM { get; } = new();
+    public void Dispose() => FramingTabVM.Dispose();
+
+    partial void OnTabIdxChanged(byte value) =>
+        previewVM.CurFXTabVM = value switch { 1 => SatTabVM, 2 => FramingTabVM, _ => null };
+
+    #region 路径
+
+    [ObservableProperty] private string? _imgPath;
+    public ObservableCollection<string> ImgPaths { get; } = [];
+    private bool ImgPathSelected => ImgPath is {};
+    private bool HasImgPath => ImgPaths.Count > 0;
+
+    partial void OnImgPathChanged(string? value) {
+        _ = previewVM.SetSrcAsync(value);
+        RemoveImgCommand.NotifyCanExecuteChanged();
+    }
+
+    public void AddImg(IEnumerable<string> paths) {
+        foreach (var path in paths.Where(path => !ImgPaths.Contains(path))) ImgPaths.Add(path);
+        RunCommand.NotifyCanExecuteChanged();
+    }
+
+    [RelayCommand]
+    private void AddImg() => Dialog.RunOrShowEx("添加图像", () => AddImg(Dialog.PickImg("添加图像", true)));
+
+    [RelayCommand(CanExecute = nameof(ImgPathSelected))]
+    private void RemoveImg() =>
+        Dialog.RunOrShowEx(
+            "移除图像",
+            () => {
+                ImgPaths.Remove(ImgPath!);
+                RunCommand.NotifyCanExecuteChanged();
+            });
+
+    #endregion 路径
+
+    #region 批处理
+
+    [ObservableProperty] private bool _idle = true;
+
+    [RelayCommand(CanExecute = nameof(HasImgPath), IncludeCancelCommand = true)]
+    private Task RunAsync(CT ct) =>
+        Dialog.RunOrShowExAsync(
+            "批处理",
+            async () => {
+                previewVM.CancelAll();
+                Idle = false;
+                var save = Saving.Saver(SavingTabVM.Format, SavingTabVM.Option);
+                while (ImgPaths is [var path, ..]) {
+                    using MImg img = new();
+                    await img.ReadAsync(path, ct);
+                    if (SatTabVM.Enabled) await Task.Run(() => SatTabVM.Apply(img, ct), ct);
+                    if (FramingTabVM.Enabled) await Task.Run(() => FramingTabVM.Apply(img, ct), ct);
+                    img.Quality = SavingTabVM.Quality;
+                    await save(img, path, ct);
+                    ImgPaths.RemoveAt(0);
+                }
+                Idle = true;
+                Dialog.ShowInfo("完成", "全部处理完成");
+            },
+            () => Idle = true);
+
+    #endregion 批处理
+}
--- GarthImgLab/VMs/PreviewVM.cs
+++ GarthImgLab/VMs/PreviewVM.cs
@@ -1,9 +1,142 @@
 namespace GarthImgLab.VMs;
 
+using System.ComponentModel;
 using System.Windows.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Core;
+using ImageMagick;
+using TabVMs;
 
-internal sealed partial class PreviewVM: ObservableObject
+internal sealed partial class PreviewVM: ObservableObject, IDisposable
 {
-    [ObservableProperty] private BitmapSource? _preview;
+    #region 属性和响应
+
+    [ObservableProperty] private BitmapSource? _bef, _aft, _preview;
+    [ObservableProperty] private FXTabVM? _curFXTabVM;
+    [ObservableProperty] private MImg? _thumb;
+    [ObservableProperty] private bool _visible;
+    private bool Active => CurFXTabVM is {} && Visible;
+    partial void OnBefChanged(BitmapSource? value) => SetPreview();
+    partial void OnAftChanged(BitmapSource? value) => SetPreview();
+    partial void OnVisibleChanged(bool value) => SetPreview();
+
+    partial void OnCurFXTabVMChanged(FXTabVM? oldValue, FXTabVM? newValue) {
+        oldValue?.PropertyChanged -= OnFxPropChanged;
+        newValue?.PropertyChanged += OnFxPropChanged;
+        if (Active) _ = GenAftAsync();
+    }
+
+    private void OnFxPropChanged(object? s, PropertyChangedEventArgs e) {
+        if (e.PropertyName == nameof(FXTabVM.Enabled))
+            SetPreview();
+        else
+            _ = GenAftAsync();
+    }
+
+    partial void OnThumbChanged(MImg? oldValue, MImg? newValue) {
+        oldValue?.Dispose();
+        _ = (GenBefAsync(), GenAftAsync());
+    }
+
+    #endregion 属性和响应
+
+    #region 打断和销毁
+
+    private CTS _srcCts = new(), _befCts = new(), _aftCts = new();
+    public void CancelAll() => _ = (RenewSrc(), RenewBef(), RenewAft());
+
+    private async Task<CT> RenewSrc() {
+        await _srcCts.CancelAsync();
+        _srcCts.Dispose();
+        return (_srcCts = new()).Token;
+    }
+
+    private async Task<CT> RenewBef() {
+        await _befCts.CancelAsync();
+        _befCts.Dispose();
+        return (_befCts = new()).Token;
+    }
+
+    private async Task<CT> RenewAft() {
+        await _aftCts.CancelAsync();
+        _aftCts.Dispose();
+        return (_aftCts = new()).Token;
+    }
+
+    public void Dispose() {
+        _srcCts.Cancel();
+        _befCts.Cancel();
+        _aftCts.Cancel();
+        _srcCts.Dispose();
+        _befCts.Dispose();
+        _aftCts.Dispose();
+        Thumb?.Dispose();
+        CurFXTabVM?.PropertyChanged -= OnFxPropChanged;
+    }
+
+    #endregion 打断和销毁
+
+    #region 渲染方法
+
+    public Task SetSrcAsync(string? path) =>
+        Dialog.RunOrShowExAsync(
+            "加载预览源",
+            async () => {
+                var ct = await RenewSrc();
+                Thumb = path is {}
+                    ? await Task.Run(
+                        async () => {
+                            MImg img = new();
+                            await img.ReadAsync(path, ct);
+                            img.ToThumb(1 << 18);
+                            return img;
+                        },
+                        ct)
+                    : null;
+            });
+
+    private Task GenBefAsync() =>
+        Dialog.RunOrShowExAsync(
+            "生成原图预览",
+            async () => {
+                Bef = Thumb is {}
+                    ? await Task.Run(
+                        () => {
+                            var bef = Thumb.ToBitmapSource();
+                            bef.Freeze();
+                            return bef;
+                        },
+                        await RenewBef())
+                    : null;
+            });
+
+    private Task GenAftAsync() =>
+        Dialog.RunOrShowExAsync(
+            "生成效果预览",
+            async () => {
+                var ct = await RenewAft();
+                if (Active && CurFXTabVM is {} vm)
+                    Aft = Thumb is {}
+                        ? await Task.Run(
+                            () => {
+                                using MImg clone = new(Thumb);
+                                ct.ThrowIfCancellationRequested();
+                                vm.Apply(clone, ct);
+                                ct.ThrowIfCancellationRequested();
+                                var aft = clone.ToBitmapSource();
+                                aft.Freeze();
+                                return aft;
+                            },
+                            ct)
+                        : null;
+            });
+
+    private void SetPreview() =>
+        Preview = Active
+            ? CurFXTabVM!.Enabled
+                ? Aft
+                : Bef
+            : null;
+
+    #endregion 渲染方法
 }
