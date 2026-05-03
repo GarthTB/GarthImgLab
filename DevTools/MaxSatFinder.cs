d9626740d19a6f4c18b11759d913a077cb0644ad
Garth TB | 天卜 <g-art-h@outlook.com>
2026-05-04 05:12:49 +0800

    重测最大色度：结果不变

--- DevTools/MaxSatFinder.cs
+++ DevTools/MaxSatFinder.cs
@@ -1,17 +1,17 @@
 namespace DevTools;
 
-using CS = Wacton.Unicolour.ColourSpace;
-using Uc = Wacton.Unicolour.Unicolour;
+using System.Collections.Concurrent;
+using Wacton.Unicolour;
 
 /* 12bit结果
-Hsb    (4095,0,0)       1
-Hsl    (4095,0,0)       1
-Hsi    (4095,0,0)       1
+Hsb    (0,4080,0)       1
+Hsl    (0,4080,0)       1
+Hsi    (0,4080,0)       1
 Lchab  (0,0,4095)       133.80761432012983
 Lchuv  (4095,0,0)       179.04142708939614
 Hsluv  (4095,4095,0)    100.05922027251566
 Hpluv  (4095,4095,0)    1784.328864093446
-Tsl    (4095,0,0)       1
+Tsl    (0,4080,0)       1
 Jzczhz (0,0,4095)       0.19027906590136512
 Oklch  (4095,0,4095)    0.32249096477516476
 Okhsv  (0,631,3101)     1.0119794128679827
@@ -20,15 +20,29 @@ Oklrch (4095,0,4095)    0.32249096477516476
 Hct    (4095,0,0)       113.35620829574427 */
 
 internal static class MaxSatFinder {
-    private const int MaxVal = (1 << 12) - 1;
+    private const int Max = (1 << 12) - 1;
+    private const double InvMax = 1d / Max;
 
     public static void Run() {
-        var maxSatArr = GetRgb()
-            .AsParallel()
-            .Aggregate(
-                static () => Enumerable.Repeat(((0d, 0d, 0d), S: double.MinValue), 14).ToArray(),
-                static (maxS, rgb) => {
-                    Uc uc = new(CS.Rgb, rgb.R / MaxVal, rgb.G / MaxVal, rgb.B / MaxVal);
+        var global = Enumerable.Repeat((.0, .0, .0, S: double.NegativeInfinity), 14).ToArray();
+
+        Parallel.ForEach(
+            Partitioner.Create(0, Max + 1),
+            static () => Enumerable.Repeat((.0, .0, .0, S: double.NegativeInfinity), 14).ToArray(),
+            static (range, _, local) => {
+                for (var (i, end) = range; i < end; i++)
+                for (var j = 0; j <= Max; j++) {
+                    Calc(0, i, j);
+                    Calc(i, 0, j);
+                    Calc(i, j, 0);
+                    Calc(Max, i, j);
+                    Calc(i, Max, j);
+                    Calc(i, j, Max);
+                }
+                return local;
+
+                void Calc(double r, double g, double b) {
+                    Unicolour uc = new(ColourSpace.Rgb, r * InvMax, g * InvMax, b * InvMax);
                     Update(0, uc.Hsb.S);
                     Update(1, uc.Hsl.S);
                     Update(2, uc.Hsi.S);
@@ -43,37 +57,19 @@ internal static class MaxSatFinder {
                     Update(11, uc.Okhsl.S);
                     Update(12, uc.Oklrch.C);
                     Update(13, uc.Hct.C);
-                    return maxS;
 
                     void Update(int i, double s) {
-                        if (s > maxS[i].S) maxS[i] = (rgb, s);
+                        if (s > local[i].S) local[i] = (r, g, b, s);
                     }
-                },
-                static (globalMaxS, localMaxS) => {
+                }
+            },
+            local => {
+                lock (global)
                     for (var i = 0; i < 14; i++)
-                        if (localMaxS[i].S > globalMaxS[i].S)
-                            globalMaxS[i] = localMaxS[i];
-                    return globalMaxS;
-                },
-                static x => x);
-        foreach (var ((r, g, b), s) in maxSatArr) Console.WriteLine($"({r},{g},{b})\t{s}");
-    }
+                        if (local[i].S > global[i].S)
+                            global[i] = local[i];
+            });
 
-    private static IEnumerable<(double R, double G, double B)> GetRgb() {
-        for (var g = 0; g <= MaxVal; g++)
-        for (var b = 0; b <= MaxVal; b++) {
-            yield return (0, g, b);
-            yield return (MaxVal, g, b);
-        }
-        for (var r = 1; r < MaxVal; r++)
-        for (var b = 0; b <= MaxVal; b++) {
-            yield return (r, 0, b);
-            yield return (r, MaxVal, b);
-        }
-        for (var r = 1; r < MaxVal; r++)
-        for (var g = 1; g < MaxVal; g++) {
-            yield return (r, g, 0);
-            yield return (r, g, MaxVal);
-        }
+        foreach (var (r, g, b, s) in global) Console.WriteLine($"({r},{g},{b})\t{s}");
     }
 }
