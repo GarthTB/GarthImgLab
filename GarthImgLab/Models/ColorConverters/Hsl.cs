b97ca3a930ff54e462444feb2432221dff8de883
Garth TB | 天卜 <g-art-h@outlook.com>
2026-07-06 00:16:26 +0800

    添加 Hsl 和 Hsv 色空间

---
+++ GarthImgLab/Models/ColorConverters/Hsl.cs
@@ -0,0 +1,67 @@
+// ReSharper disable CompareOfFloatsByEqualityOperator
+
+namespace GarthImgLab.Models.ColorConverters;
+
+using static Math;
+
+public static class Hsl {
+    public static (double H, double S, double L) FromSRgb(double r, double g, double b) {
+        var max = Max(r, Max(g, b));
+        var min = Min(r, Min(g, b));
+        var c = max - min;
+        var l = (max + min) / 2;
+
+        var h = 0d;
+        if (c != 0) {
+            if (max == r)
+                h = 60 * ((g - b) / c % 6);
+            else if (max == g)
+                h = 60 * ((b - r) / c + 2);
+            else
+                h = 60 * ((r - g) / c + 4);
+            if (h < 0) h += 360;
+        }
+
+        var s = c == 0
+            ? 0
+            : c / (1 - Abs(2 * l - 1));
+        return (h, s, l);
+    }
+
+    public static (double R, double G, double B) ToSRgb(double h, double s, double l) {
+        h %= 360;
+        if (h < 0) h += 360;
+        var c = (1 - Abs(2 * l - 1)) * s;
+        var x = c * (1 - Abs(h / 60 % 2 - 1));
+        var m = l - c / 2;
+
+        double r, g, b;
+        if (h < 60) {
+            r = c;
+            g = x;
+            b = 0;
+        } else if (h < 120) {
+            r = x;
+            g = c;
+            b = 0;
+        } else if (h < 180) {
+            r = 0;
+            g = c;
+            b = x;
+        } else if (h < 240) {
+            r = 0;
+            g = x;
+            b = c;
+        } else if (h < 300) {
+            r = x;
+            g = 0;
+            b = c;
+        } else {
+            r = c;
+            g = 0;
+            b = x;
+        }
+
+        return (r + m, g + m, b + m);
+    }
+}
---
+++ GarthImgLab/Models/ColorConverters/Hsv.cs
@@ -0,0 +1,66 @@
+// ReSharper disable CompareOfFloatsByEqualityOperator
+
+namespace GarthImgLab.Models.ColorConverters;
+
+using static Math;
+
+public static class Hsv {
+    public static (double H, double S, double V) FromSRgb(double r, double g, double b) {
+        var max = Max(r, Max(g, b));
+        var min = Min(r, Min(g, b));
+        var c = max - min;
+
+        var h = 0d;
+        if (c != 0) {
+            if (max == r)
+                h = 60 * ((g - b) / c % 6);
+            else if (max == g)
+                h = 60 * ((b - r) / c + 2);
+            else
+                h = 60 * ((r - g) / c + 4);
+            if (h < 0) h += 360;
+        }
+
+        var s = max == 0
+            ? 0
+            : c / max;
+        return (h, s, max);
+    }
+
+    public static (double R, double G, double B) ToSRgb(double h, double s, double v) {
+        h %= 360;
+        if (h < 0) h += 360;
+        var c = v * s;
+        var x = c * (1 - Abs(h / 60 % 2 - 1));
+        var m = v - c;
+
+        double r, g, b;
+        if (h < 60) {
+            r = c;
+            g = x;
+            b = 0;
+        } else if (h < 120) {
+            r = x;
+            g = c;
+            b = 0;
+        } else if (h < 180) {
+            r = 0;
+            g = c;
+            b = x;
+        } else if (h < 240) {
+            r = 0;
+            g = x;
+            b = c;
+        } else if (h < 300) {
+            r = x;
+            g = 0;
+            b = c;
+        } else {
+            r = c;
+            g = 0;
+            b = x;
+        }
+
+        return (r + m, g + m, b + m);
+    }
+}
