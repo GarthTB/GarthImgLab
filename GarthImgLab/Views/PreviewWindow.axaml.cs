848b4424d3bacdfdae7d40bb323cd75111f09ae3
Garth TB | 天卜 <g-art-h@outlook.com>
2026-07-09 01:04:12 +0800

    补全发布配置

--- GarthImgLab/Views/PreviewWindow.axaml.cs
+++ GarthImgLab/Views/PreviewWindow.axaml.cs
@@ -2,13 +2,26 @@ namespace GarthImgLab.Views;
 
 using Avalonia.Controls;
 using Common;
-using ViewModels;
+using Contexts;
 
 public sealed partial class PreviewWindow: Window {
+    private static PreviewWindow? _current;
+
     public PreviewWindow() {
         InitializeComponent();
         Title = $"{Meta.Name} - 预览";
     }
 
-    public PreviewWindow(MainVm vm): this() => DataContext = vm;
+    public static void ShowOrActivate(IWorkspaceCtx ctx) {
+        if (_current is {}) {
+            _current.Activate();
+            return;
+        }
+        _current = new() { DataContext = ctx };
+        _current.Closed += (_, _) => {
+            ctx.Clear();
+            _current = null;
+        };
+        _current.Show();
+    }
 }
