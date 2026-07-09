661420a7c1c89592bdfcd9816eafbc0db717d52f
Garth TB | 天卜 <g-art-h@outlook.com>
2026-07-09 14:34:46 +0800

    添加消息弹窗

---
+++ GarthImgLab/Views/MsgWindow.axaml.cs
@@ -0,0 +1,15 @@
+namespace GarthImgLab.Views;
+
+using Avalonia.Controls;
+
+public sealed partial class MsgWindow: Window {
+    // ReSharper disable once UnusedMember.Global
+    /// <summary> 设计器专用 </summary>
+    public MsgWindow(): this("标题", "内容") {}
+
+    public MsgWindow(string title, string msg) {
+        InitializeComponent();
+        Title = title;
+        Msg.Text = msg;
+    }
+}
